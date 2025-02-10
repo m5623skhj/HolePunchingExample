6c47098ce4e6bfb9e43aa5e303c71ae1872aca1e
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2025-02-11 01:27:28 +0900

    * Add solution and projects

---
+++ HolePunchingTest/HolePunchingTest/main.cpp
@@ -0,0 +1,12 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+
+
+	return 0;
+}
---
+++ HolePunchingTest/HolePunchingTestClient/main.cpp
@@ -0,0 +1,8 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
