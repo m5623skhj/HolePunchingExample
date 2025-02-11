d84cb2f72e4a8e71c3a19c5d72d90c65bd9a2fda
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2025-02-12 00:27:40 +0900

    * 홀펀칭 테스트를 위한 기본 코드 추가

--- HolePunchingTest/HolePunchingTest/main.cpp
+++ HolePunchingTest/HolePunchingTest/main.cpp
@@ -4,9 +4,45 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
+using namespace std;
+
+constexpr short port = 9500;
+
 int main()
 {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        cout << "WSAStartup failed" << WSAGetLastError() << std::endl;
+        return 0;
+    }
 
+    SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == INVALID_SOCKET)
+    {
+        cout << "socket failed" << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 0;
+    }
+
+    sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+
+    if (bind(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR)
+    {
+        cout << "bind failed" << WSAGetLastError() << std::endl;
+        closesocket(sock);
+        WSACleanup();
+        return 0;
+    }
+
+    // do what?
+
+
+    closesocket(sock);
+    WSACleanup();
 
 	return 0;
 }
--- HolePunchingTest/HolePunchingTestClient/main.cpp
+++ HolePunchingTest/HolePunchingTestClient/main.cpp
@@ -1,8 +1,55 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
+#include <string>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
+
+constexpr short port = 9500;
+constexpr const char* serverIp = "127.0.0.1";
 
 int main()
 {
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		cout << "WSAStartup failed " << WSAGetLastError() << std::endl;
+		return 0;
+	}
+
+    SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		cout << "socket failed" << WSAGetLastError() << std::endl;
+		WSACleanup();
+		return 0;
+	}
+
+    sockaddr_in serverAddr = {};
+    serverAddr.sin_family = AF_INET;
+    inet_pton(AF_INET, serverIp, &serverAddr.sin_addr);
+    serverAddr.sin_port = htons(port);
+
+    sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(0);
+
+	if (bind(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR)
+	{
+		cout << "bind failed" << WSAGetLastError() << std::endl;
+		closesocket(sock);
+		WSACleanup();
+		return 0;
+	}
+
+	// do what?
+
+
+	closesocket(sock);
+	WSACleanup();
+
 	return 0;
 }
